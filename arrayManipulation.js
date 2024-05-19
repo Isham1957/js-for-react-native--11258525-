8499f11a0d30a2ab6dbfbc000169ba6b6c22ae1c
Isham1957 <isowah004@st.ug.edu.gh>
2024-05-19 14:17:39 -0700

    Completed task2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,11 +1,26 @@
 function processArray(numbers) {
     return numbers.map(num => {
       if (num % 2 === 0) {
-        return num * num; 
+        return num * num; // square even numbers
       } else {
-        return num * 3; 
+        return num * 3; // triple odd numbers
       }
     });
   }
   const numbers = processArray([1, 2, 3, 4, 5]);
   console.log(numbers);
+  
+  
+  function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+      if (numbers[index] % 2 === 0) {
+        return str.toUpperCase();
+      } else {
+        return str.toLowerCase();
+      }
+    });
+  }
+  export const words = ["Isham", "Sowah", "Fati", "Latifa"];
+  export const strings = formatArrayStrings(words , numbers);
+  console.log(strings);
+  
